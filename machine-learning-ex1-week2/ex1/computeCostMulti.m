92b16d377c2c6e73e546dd71949ca1b63b18d9f7
zhaozhaoyan <2451819992@qq.com>
2017-10-08 19:09:53 +0800

    编程作业week2

--- machine-learning-ex1-week2/ex1/computeCostMulti.m
+++ machine-learning-ex1-week2/ex1/computeCostMulti.m
@@ -6,13 +6,15 @@ function J = computeCostMulti(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
+H=X*theta-y;
+Sigma=sum(H.^2,1);
+J=Sigma/(2*m);
 
 
 
